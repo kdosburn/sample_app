7a12dbf7bd9383a5d5eba07d9bf98ca80b6c0c82
Kendra Osburn <kdosburn@gmail.com>
2012-04-22 15:24:15 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,14 +8,15 @@ describe "User pages" do
     before { visit signup_path }
   end
 
- describe "profile page" do
+  describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
     it { should have_selector('title', text: user.name) }
   end
-describe "signup" do
+
+  describe "signup" do
 
     before { visit signup_path }
 
@@ -25,19 +26,35 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-    end
+  end
 
-    describe "with valid information" do
+  describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
+  
+    describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
 
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+        describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+      
+      end
+      
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+  
     end
   end
 end
